ee2172f0493ce4eb9dafe0eefd8b5018eea2768a
Phillip Wirth <schaum@kaffeekronde.de>
2015-12-09 20:18:21 +0100

    added tests

---
+++ tests.py
@@ -0,0 +1,14 @@
+#tests.py
+import unittest
+from add_two_numbers import add_two_numbers
+
+class TestsForAddFunction(unittest.TestCase):
+
+    def test_zeros(self):
+        result = add_two_numbers(0, 0)
+        self.assertEqual(0, result)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
